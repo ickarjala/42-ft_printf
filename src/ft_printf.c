9fe3aa994caaa6f07fecf584378ba2867a82963e
ikarjala <ikarjala@student.hive.fi>
2022-07-25 20:02:47 +0300

    prototype basic structure

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,27 +6,58 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 15:47:44 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/06/30 17:35:04 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/07/25 20:01:56 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	parse_flag(char *format, va_list *ap)
+static int	parse_flag(char *format, va_list ap)
 {
-	
+	if (*format == FLAG_PREFIX)
+		//literal
+		return (1);
+	return (0);
 }
 
+static char	*bufcat(t_strinfo data)
+{
+	char	*str;
+	t_bufl	*link;
+
+	str = (char *)malloc(sizeof(char) * data.len);
+	link = data.fst;
+	while (link)
+	{
+		// memmove
+		link = link->next;
+	}
+}
+
+/* iterate through format, finding % flag
+ * collect flags info and pass to dispatcher
+ * process variables and store a buffer for the flag output
+ * append all the buffers together into 1 string, freeing memory as you go
+ * write out
+*/
 int	printf(const char *format, ...)
 {
-	va_list	ap;
+	va_list		ap;
+	t_strinfo	data;
 
+	data = (t_strinfo){NULL, NULL, 0, 0};
 	va_start(ap, format);
 	while (*format)
 	{
 		if (*format == FLAG_PREFIX)
-
+			parse_flag(++format, ap);
 		format++;
 	}
+	if (data.evals)
+		data.buf = bufcat(data);
+	write(FD_OUT, data.buf, data.len);
+	free(data.buf); // replace with protected fn
 	va_end(ap);
 }
+
+// hello%%hu -> [hello]:[%hu]
--- src/ft_printf.h
+++ src/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 15:46:33 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/06/30 17:31:38 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/07/25 19:44:58 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,28 @@
 
 # define FLAG_PREFIX	'%'
 
+# define FD_IN	0
+# define FD_OUT	1
+# define FD_ERR	2
+
 typedef struct s_string_info
 {
-	int	len;
+	s_buffer_chain	*fst;
+	char			*buf;
+	size_t			len;
+	int				evals;
 }	t_strinfo;
+
+typedef struct s_buffer_chain
+{
+	char			*cont;
+	size_t			len;
+	s_buffer_chain	*next;
+}	t_bufl;
+
+typedef struct s_flag_info
+{
+	int	padding;
+}	t_flaginfo;
 
 #endif
